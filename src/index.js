d05e81a4df2550874104b28e2be1a2a7ea4232c6
Thiago Henrique <thiagohp070@gmail.com>
2023-11-30 13:22:53 -0300

    first commit

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.json({ hello: "world" });
-});
-
-app.listen(3333, () => console.log("server running on port 3333"));
